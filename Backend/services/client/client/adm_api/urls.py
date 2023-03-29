0cf4275b480b0ce7605757102bd9910b116f65e3
BrunoTumelero <Bruno.Tumelero@outlook.com>
2023-03-29 18:29:12 -0300

    endpoints of criation and edition product and category

--- Backend/services/client/client/adm_api/urls.py
+++ Backend/services/client/client/adm_api/urls.py
@@ -5,5 +5,7 @@ from .views import *
 app_name ="adm"
 
 urlpatterns = [
-    re_path(r'^create_user$', create_user, name='create_user'),   
+    re_path(r'^create_new_product$', create_new_product, name='create_new_product'), 
+    re_path(r'^edit_product/(?P<pk>\d+)$', edit_product, name='edit_product'),   
+    re_path(r'^create_product_category$', create_product_category, name='create_product_category'),   
 ]
--- Backend/services/client/client/adm_api/views.py
+++ Backend/services/client/client/adm_api/views.py
@@ -7,53 +7,73 @@ from django.conf import settings
 from client.register.models import *
 
 from .utils import _create_token
+from .forms import ProductsForm, ProductCategoryForm
 
 @csrf_exempt
-def create_user(request):
-    user_email = request.POST.get('user_email', None)
-    password = request.POST.get('password', None)
-    type_user = request.POST.get('type_user', None)
-    full_name = request.POST.get('full_name', None)
-    phone = request.POST.get('phone', None)
-    cpf = request.POST.get('cpf', None)
+def create_new_product(request):
+    data = request.POST.copy()
 
-    if full_name and user_email and password:
-        #Check email already exists
-        if User.objects.filter(email=user_email).exists():
-            try:
-                user = User.objects.get(email=user_email)
-            except User.MultipleObjectsReturned:
-                return JsonResponse({'message': 'Email já cadastrado', 'status': 400})
-            if not user.is_company:
-                return JsonResponse({'message': 'Email já cadastrado', 'status': 400})
-        else:
-            #create user
-            if type_user == 'client':
-                try:
-                    user = User.objects.create(email=user_email)
-                    user.email = user_email
-                    user.set_password(password)
-                    user.is_active = True
-                    user.is_client = True
-                    user.activation_key = _create_activation_token()
-                    session_token = _create_token(user)
-                    user.save()
-                    client, _ = Clients.objects.get_or_create(user=user)
-                    client.full_name = full_name
-                    client.whatsapp = phone
-                    client.cpf = cpf
-                    client.save()
-
-                    return JsonResponse({'message': 'Usuário cadastrado com sucesso', 'id': user.id, 'token': session_token, 'status': 200})
-                except IntegrityError:
-                    #email duplicate
-                    return JsonResponse({'message': 'Emais já cadastrado', 'status': 400})
-
-            if type_user == 'company':
-                return JsonResponse({'message': 'Cadastro para usuários', 'status': 400})
+    if data['name']:
+        try:
+            product = Products.objects.get(name=data['name'])
+            form = ProductsForm(instance=product, data=data)
+        except Products.DoesNotExist:
+            form = ProductsForm(data=data)
         
-            else:
-                return JsonResponse({'message': 'Tipo inválido', 'status': 400})
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'message': 'Produto adicionado com sucesso', 'status': 200})
+        else:
+            print(form.errors)
+            return JsonResponse({'message': 'Erro ao salvar o produto', 'error': form.errors, 'status':404})
+    
+    return JsonResponse({'message': 'Erro ao adicionar o produto', 'status': 400})
 
-        return JsonResponse({'message': 'Erro ao cadastrar usuário', 'status': 400})
-    return JsonResponse({'message': 'Informe os campos obrigatórios', 'status': 400})
+@csrf_exempt
+def edit_product(request, pk):
+    name = request.POST.get('name', None)
+    description = request.POST.get('description', None)
+    category = request.POST.get('category', None)
+    price = request.POST.get('price', None)
+    is_avaliable = request.POST.get('is_avaliable', False)
+    
+    if is_avaliable == 'true' or is_avaliable == 'True':
+        is_avaliable = True
+        
+    if pk:
+        if category:
+            category = ProductCategory.objects.get(pk=category)
+        product = Products.objects.get(pk=pk)
+        product.company = request.user.company
+        product.name = name
+        product.description = description
+        product.category = category
+        product.price = price
+        product.is_avalable = is_avaliable
+        
+        product.save()
+        
+        return JsonResponse({'message': 'Produto alterado com sucesso', 'status': 200})
+        
+    return JsonResponse({'message': 'Produto inválido', 'status': 400})
+
+@csrf_exempt
+def create_product_category(request):
+    data = request.POST.copy()
+
+    if data['category']:
+        try:
+            cat = ProductCategory.objects.get(category=data['category'])
+            form = ProductCategoryForm(instance=cat, data=data)
+            if form.is_valid():
+                form.save()
+                return JsonResponse({'message': 'Categoria adicionada comsucesso', 'status': 200})
+        except ProductCategory.DoesNotExist:
+            form = ProductCategoryForm(data=data)
+            if form.is_valid():
+                form.save()
+                return JsonResponse({'message': 'Categoria adicionada comsucesso', 'status': 200})
+            else:
+                print(form.errors)
+                return JsonResponse({'message': 'Erro', 'status': 400})
+    return JsonResponse({'message': 'Informe a categoria', 'status': 400})
